914c648e8249bd8ad13e5329a95409f7bd3aa48f
skrnvn <skrnvn@gmail.com>
2019-12-03 15:32:30 +0200

    Add home page draft

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+$("#slider").slick({
+    variableWidth: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    dotsClass: 'my-dots',
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  })
+
+  
